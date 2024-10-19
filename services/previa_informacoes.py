e1b86f8bbc259dad5a90cc1b3585a0398423da5d
RubensLFerreira <rubenslima000@gmail.com>
2024-10-19 20:24:57 -0300

    refactor: refatoração da estrutura do projeto

--- services/previa_informacoes.py
+++ services/previa_informacoes.py
@@ -8,8 +8,8 @@ def previa_informacoes(df):
 
 	st.write(f"Total de linhas: {df.shape[0]}")
 	st.write(f"Total de colunas: {df.shape[1]}")
-
 	st.subheader("Distribuição por grau de instrução")
 	st.write("Distribuição por Grau de Instrução")
+	
 	grau_instrucao_count = df.groupby('DS_GRAU_INSTRUCAO').size().reset_index(name='count')
 	st.bar_chart(grau_instrucao_count.set_index('DS_GRAU_INSTRUCAO'), stack=False)
